0d649b4c75d2bab64c056e5de51947feed0b27c3
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-05-28 12:59:56 -0400

    new repo stuff

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './App.css';
+import SizingForm from "./components/sizing-form.js";
+
+function App() {
+  return (
+    <div className="App">      
+      <SizingForm />
+    </div>
+  );
+}
+
+export default App;
