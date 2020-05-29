d710ed66465ed7a5c194a65ddb9596eda23be3c6
k-lord <katielord27@gmail.com>
2020-05-29 15:11:40 -0400

    set up container dir but ended up only coding in login.js component - states set up but need to add fetch and auth functions

---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,57 @@
+// import React from 'react';
+// import { LogIn } from '../components/login';
+
+// class LogInContainer extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             email = '',
+//             password = '',
+//             authorized = false
+//         };
+//         this.authorize = this.authorize.bind(this);
+//         this.handleEmailChange = this.handleEmailChange.bind(this);
+//         this.handlePasswordChange = this.handlePasswordChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+
+//     handleEmailChange(e) {
+//         this.setState({
+//             email: e.target.value
+//         });
+//         console.log(this.state.email);
+//     }
+
+//     handlePasswordChange(e) {
+//         this.setState({
+//             password: e.target.value
+//         });
+//         console.log(this.state.password);
+
+//     }
+
+//     handleSubmit(e) {
+
+//     }
+
+//     componentDidMount() {
+//         // fetch will go here
+//     };
+
+//     authorize(e) {
+//         const email = "";
+//         const password = "";
+//         const auth = password == this.state.password;
+//         this.setState({authorized: auth});
+//     }
+
+
+// }
+
+// ReactDOM.render(
+//     <LogIn />,
+//     document.getElementById()
+// )
+
+
+
