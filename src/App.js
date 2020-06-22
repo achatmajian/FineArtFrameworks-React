76c1b453900f4a42a1a772bd27a7dd80053bd11d
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-06-22 12:40:45 -0400

    cleaned up some files and folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 // import './App.css';
-import SizingForm from "./components/sizing-form.js";
 import Navigation from "./components/nav.js";
 import ViewTable from "./components/view-table.js";
 import ViewClients from "./components/view-clients.js";
@@ -12,14 +11,6 @@ import ViewSpacers from "./components/view-spacers.js";
 import ViewExtras from "./components/view-extras.js";
 import ViewOrders from "./components/view-orders.js";
 import LogIn from "./components/login.js";
-import NewClientForm from "./components/new-client-form.js";
-import CreateOrderHeader01 from './components/create-order-header-01.js';
-import OrderDetailsForm from './components/order-details-form.js';
-import CreateOrderHeader02 from './components/create-order-header-02.js';
-import SizingForm2 from './components/sizing-form-2.js';
-import ManufacturingForm from './components/manufacturing-form.js';
-import FittingForm from './components/fitting-form.js';
-import NotesForm from './components/notes-form.js';
 import BuildFramePage from './components/03-build-frame-page.js';
 import OrderDetailsPage from './components/02-order-details-page.js';
 import NewClientPage from './components/01-new-client-page.js';
@@ -29,30 +20,28 @@ function App() {
   return (
     <div className="App">
       <Navigation />      
-      {/* <SizingForm /> */}
       {/* <ViewTable /> */}
-      {/* <ViewClients /> */}
-      {/* <ViewUsers /> */}
-      {/* <ViewLumber /> */}
-      {/* <ViewGlazing /> */}
-      {/* <ViewMounting /> */}
-      {/* <ViewSpacers /> */}
-      {/* <ViewExtras /> */}
-      {/* <ViewOrders /> */}
+      {/* <ViewClients />
+      <ViewUsers />
+      <ViewLumber />
+      <ViewGlazing />
+      <ViewMounting />
+      <ViewSpacers />
+      <ViewExtras />
+      <ViewOrders /> */}
 
       {/* Log In "page" */}
       {/* <LogIn /> */}
       
       {/* First "page" in the "Create Order" flow, where users enter client info */}
       {/* <NewClientPage /> */}
-      <ExistingClientPage />
-
+      {/* <ExistingClientPage /> */}
 
       {/* Second "page" in the "Create Order" flow, where users enter order info. */}
       {/* <OrderDetailsPage /> */}
 
       {/* Third "page" in the "Create Order" flow, where users build a frame. */}
-      {/* <BuildFramePage /> */}
+      <BuildFramePage />
 
     </div>
   );
