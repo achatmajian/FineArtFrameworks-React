ebffbcae7ba0d4839cd33cf82cc47c3363b875dd
Ace Yogurt <jeremygill87@gmail.com>
2020-06-24 21:13:41 -0400

    API call for existing client table is working

--- src/components/view-clients.js
+++ src/components/view-clients.js
@@ -1,93 +1,107 @@
 import React from "react";
-import { Table, Container, Row, Button, InputGroup, FormControl} from "react-bootstrap";
+import { Table, Container, Row, Button, InputGroup, FormControl } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./view-table.css";
 
 
 class ViewClients extends React.Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = { clients: [] };
+  }
+  componentDidMount() {
+    this.fetchClients();
+  }
+
+  fetchClients = () => {
+    fetch("http://18.222.181.253:3001/api/clients")
+      .then(response => response.json())
+      .then(response => this.handleSuccessResponse(response))
+      .catch(error => this.handleErrorResponse(error));
+  };
+
+  handleSuccessResponse = response => {
+    this.setState({ clients: response }, () => {
+      console.log(this.state.clients, "clients");
+    });
+  }
+
+  handleErrorResponse = error => {
+    console.log("Error while fetching clients: ", error);
+  }
+
+  renderTableData() {
+    return this.state.clients.map((client, index) => {
+      const { id, first_name, last_name, email, address_one, address_two, city, state, zip_code } = client
       return (
-        <div className="wrapper">
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{first_name}</td>
+          <td>{last_name}</td>
+          <td>{email}</td>
+          <td>{address_one}</td>
+          <td>{address_two}</td>
+          <td>{city}</td>
+          <td>{state}</td>
+          <td>{zip_code}</td>
+        </tr>
+      )
+    })
+  }
+
+  render() {
+    return (
+      // <div>
+      //   <h1 id="title">Some Bullshit</h1>
+      //   <table id="clients">
+      //     <tbody>
+      //       {this.renderTableData()}
+      //     </tbody>
+      //   </table>
+      // </div>
+      <div className="wrapper">
         <Container className="table-container" fluid>
           <h2 className="page-title">View Clients</h2>
           <Row>
 
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder="Search Clients"
-              aria-label="Search Clients"
-              aria-describedby="basic-addon2"
-            />
-            <InputGroup.Append>
-              <InputGroup.Text id="basic-addon2">Search</InputGroup.Text>
-            </InputGroup.Append>
-          </InputGroup>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Search Clients"
+                aria-label="Search Clients"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <InputGroup.Text id="basic-addon2">Search</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
             <Table className="table" responsive>
-            <thead>
+              <thead>
                 <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Address</th>
-                <th>Apartment</th>
-                <th>City</th>
-                <th>State</th>
-                <th>Zip</th>
-                <th>Edit</th>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Address</th>
+                  <th>Apartment</th>
+                  <th>City</th>
+                  <th>State</th>
+                  <th>Zip</th>
+                  <th>Edit</th>
                 </tr>
-            </thead>
-            <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Arek</td>
-                  <td>Chatmajian</td>
-                  <td>arek.chatmajian@gmail.com</td>
-                  <td>201-397-4499</td>
-                  <td>148 Frost St</td>
-                  <td>3L</td>
-                  <td>Brooklyn</td>
-                  <td>NY</td>
-                  <td>11211</td>
-                  <td><Button variant="primary" size="sm">Edit</Button>{' '}</td>
-                </tr>
-                <tr>
-                <td>2</td>
-                  <td>Arek</td>
-                  <td>Chatmajian</td>
-                  <td>arek.chatmajian@gmail.com</td>
-                  <td>201-397-4499</td>
-                  <td>148 Frost St</td>
-                  <td>3L</td>
-                  <td>Brooklyn</td>
-                  <td>NY</td>
-                  <td>11211</td>
-                  <td><Button variant="primary" size="sm">Edit</Button>{' '}</td>
-                </tr>
-                <tr>
-                <td>3</td>
-                  <td>Arek</td>
-                  <td>Chatmajian</td>
-                  <td>arek.chatmajian@gmail.com</td>
-                  <td>201-397-4499</td>
-                  <td>148 Frost St</td>
-                  <td>3L</td>
-                  <td>Brooklyn</td>
-                  <td>NY</td>
-                  <td>11211</td>
-                  <td><Button variant="primary" size="sm">Edit</Button>{' '}</td>
-                </tr>
-            </tbody>
-            
+              </thead>
+              <tbody>
+                {this.renderTableData()}
+              </tbody>
+
             </Table>
-        </Row>
-</Container>
-</div>
-  
-      );
-    }
+          </Row>
+        </Container>
+      </div>
+
+    );
   }
+}
 
 
 export default ViewClients;
